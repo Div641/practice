d5cde1c46a5a2a54aaa1e5310c9db71929aef1a0
Divyanka <divyanka641@gmail.com>
2025-11-21 15:13:51 +0530

    ipl team color display game

--- script.js
+++ script.js
@@ -1,10 +1,72 @@
 var btn = document.querySelector("button");
 var box = document.querySelector(".box");
+var h1 = document.querySelector("h1");
+var main = document.querySelector(".main");
+
+var arr=[
+    {
+        team:"RCB",
+        primary:"red",
+        secondary:"green"
+    },
+    {
+        team:"CSK",
+        primary:"yellow",
+        secondary:"blue"
+    },
+
+    {
+        team:"MI",
+        primary:"blue",
+        secondary:"silver"
+    },
+
+    {
+        team:"PBSK",
+        primary:"crimson",
+        secondary:"silver"
+    },
+
+    {
+        team:"SRH",
+        primary:"orange",
+        secondary:"black"
+    },
+
+    {
+        team:"KKR",
+        primary:"purple",
+        secondary:"yellow"
+    },
+
+    {
+        team:"RR",
+        primary:"pink",
+        secondary:"blue"
+    },
+
+    {
+        team:"GT",
+        primary:"dark blue",
+        secondary:"gold"
+    },
+
+    {
+        team:"LSG",
+        primary:"light blue",
+        secondary:"orange"
+    },
+    
+    {
+        team:"DC",
+        primary:"blue",
+        secondary:"red"
+    }
+]
 
 btn.addEventListener("click", function() {
-    var c1= Math.floor(Math.random() * 256);
-    var c2= Math.floor(Math.random() * 256);
-    var c3= Math.floor(Math.random() * 256);
-
-    box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+    var winner=arr[ Math.floor(Math.random()*arr.length) ];
+    h1.innerText=winner.team;
+    main.style.backgroundColor=winner.primary;
+    box.style.backgroundColor=winner.secondary;
 });
