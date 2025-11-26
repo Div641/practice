d442879d6a9b1617b1339dbbeaf967cc64843c5a
Divyanka <divyanka641@gmail.com>
2025-11-26 18:41:11 +0530

    insta like page

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// 
-var grow=0
-var btn = document.querySelector('button');
-var h2 = document.querySelector('h2');
-var inner = document.querySelector('.inner');
+var img = document.querySelector('.card img');
+var love = document.querySelector('#love');
 
-btn.addEventListener('click', function() {
-    btn.style.pointerEvents= 'none';
+img.addEventListener('dblclick', function() {
+    love.style.opacity = 1;
+    love.style.transform = 'translate(-50% ,-50%) scale(1) rotate(0deg)';
 
-    var num = Math.floor(Math.random() *100); // to generate random number
-    console.log(num);
+    setTimeout(function() {
+        love.style.transform = 'translate(-50% ,-300%) scale(1) rotate(60deg)';
 
-    var int = setInterval(() => {
-        grow++;
-        h2.innerHTML=grow +'%';
-        inner.style.width=grow +'%';
-    },num);    //hoga 100 percent hi download bs speed change hotirahegi har alag baar
+        },800);
 
-    setTimeout(() => {
-        clearInterval(int);
-        btn.innerHTML='Downloaded';
-        btn.style.opacity='0.6';
-    },num*100);
-
-    console.log('Downloaded in '+ num/10 +'seconds ' );
+    setTimeout(function() {
+        love.style.opacity = 0
+        // love.style.transform = 'translate(-50% ,-50%) scale(0) rotate(-60deg)';
+    },1000);
+    
+    setTimeout(function() {
+        // love.style.opacity = 0
+        love.style.transform = 'translate(-50% ,-50%) scale(0) rotate(-60deg)';
+    },1200);
 });
-
